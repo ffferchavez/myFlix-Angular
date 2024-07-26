8e5bb86e165cc969e3c07d7d48e8a9180d565da5
ffferchavez <ffferchavez@gmail.com>
2024-07-26 22:35:07 +0200

    added styling on each component

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -34,7 +34,7 @@ export class WelcomePageComponent implements OnInit {
    */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
+      width: '400px'
     });
   }
 
@@ -43,7 +43,7 @@ export class WelcomePageComponent implements OnInit {
    */
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
+      width: '400px'
     });
   }
 }
